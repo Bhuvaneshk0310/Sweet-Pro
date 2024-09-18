5e88adaf1878c7eb76f7839b546a4114177f8e9f
Bhuvaneshk0310 <bhuvaneshk0310@gmail.com>
2024-09-18 09:28:19 +0530

    sweet website pro

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './NewCollections.css'
-import new_collection form '../Assets/new_collection'
+import new_collections from '../Assets/new_collections'
 const NewCollections = () => {
   return (
     <div className='new-collections'>
       <h1>New Dish</h1>
       <hr/>
       <div className='collections'>
-        {new_collection.map()}
+      {new_collections.map((item, i) => {
+        return (
+          <Item 
+            key={i} 
+            id={item.id}  
+            name={item.name} 
+            image={item.image} 
+            new_price={item.new_price} 
+            old_price={item.old_price}
+          />
+        );
+      })}
       </div>
     </div>
   )
